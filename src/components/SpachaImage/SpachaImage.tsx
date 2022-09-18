552cb84534b38903918ea6ea5cb428a052db7c17
BonyChops <contact@bonychops.com>
2022-09-19 00:14:33 +0900

    Edit: add key props

--- src/components/SpachaImage/SpachaImage.tsx
+++ src/components/SpachaImage/SpachaImage.tsx
@@ -10,15 +10,11 @@ export type SpachaImageProps = SpachaBaseProps & {
 };
 
 export const SpachaImage: FC<SpachaImageProps> = (props) => {
+  const id = `spc-canvas${props.key ? `-${props.key}` : ''}`;
   useEffect(() => {
-    const canvas = document.getElementById('spc-canvas') as HTMLCanvasElement;
+    const canvas = document.getElementById(id) as HTMLCanvasElement;
     const canvasContext = canvas.getContext('2d') as CanvasRenderingContext2D;
     new SpachaImageCore(canvasContext, props);
   }, [JSON.stringify(props)]);
-  return (
-    <canvas
-      id={`spc-canvas${props.key ? `-${props.key}` : ''}`}
-      width={600}
-    ></canvas>
-  );
+  return <canvas id={id} width={600}></canvas>;
 };
